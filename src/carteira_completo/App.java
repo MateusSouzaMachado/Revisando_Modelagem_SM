b3124d369a3ecfe41cfa2ca118340ecb082356ca
MateusSouzaMachado <mateus2005macgado@gmail.com>
2025-03-04 22:17:13 -0300

    1

---
+++ src/carteira_completo/App.java
@@ -0,0 +1,22 @@
+package carteira_completo;
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Carteira carteiraSamuel = new Carteira();
+        carteiraSamuel.receita(50);
+            
+        System.out.println(carteiraSamuel.getSaldo());
+
+        carteiraSamuel.despesa(26);
+        carteiraSamuel.despesa(0);
+        carteiraSamuel.despesa(0);
+
+        System.out.println(carteiraSamuel.getSaldo());
+
+        carteiraSamuel.despesa(0);
+        System.out.println(carteiraSamuel.getSaldo());
+    }
+}
---
+++ src/carteira_completo/CarteiraApp.java
@@ -0,0 +1,18 @@
+package carteira_completo;
+
+public class CarteiraApp {
+
+    public static void main(String[] args) {
+        Carteira carteira1 = new Carteira();
+
+        carteira1.receita(100);
+        carteira1.despesa(1000);
+        System.out.println(carteira1.getSaldo());
+
+        carteira1.despesa(60);
+        System.out.println(carteira1.getSaldo());
+
+        carteira1.despesa(20);
+        System.out.println(carteira1.getSaldo());
+    }
+}
